e34fda5c99e87e17f3a567d93c2eaa5685f39351
Carlos Muino <camumino@gmail.com>
2017-07-26 16:56:27 +0200

    support for Rails 5

--- app/controllers/redsys/tpv_controller.rb
+++ app/controllers/redsys/tpv_controller.rb
@@ -1,6 +1,6 @@
 module Redsys
   class TpvController < ApplicationController
-    skip_before_filter :verify_authenticity_token, only: [:confirmation]
+    skip_before_action :verify_authenticity_token, only: [:confirmation]
 
     #
     # Formulario de salto a la pasarela de pago
